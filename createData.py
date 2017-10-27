c833767e7418c87b5f964ce6576c762a98c913c3
Samarth Gupta <samarthgupta@SAM-Macbook-Pro.mshome.net>
2017-10-28 00:47:24 +0530

    First commit

--- createData.py
+++ createData.py
@@ -3,7 +3,7 @@ import random
 file = open("traffic.data", "a")
 for i in range(30):
     for j in range(24):
-        a = str(i+1) + ',' + str(j + 1) + ',' + str(1) + ',' + str(random.randint(0, 40)/10)
+        a = str(i+1) + ',' + str(j + 1) + ',' + str(5) + ',' + str(random.randint(0, 40)/10)
         file.write(a+'\n')
 
 
